292ef220346ba34142d60e36d50942a7271ed22b
Lars Lehtonen <lars.lehtonen@gmail.com>
2019-10-15 15:08:52 -0700

    fcc: comment the fancy fields in Station{}

--- fcc.go
+++ fcc.go
@@ -1,88 +1,88 @@
 package fcc
 
 type License struct {
-	LicenseID            string
-	SourceSystem         string
-	Callsign             string
-	FacilityID           string
-	FRN                  string
-	LicName              string
-	CommonName           string
-	RadioServiceCode     string
-	RadioServiceDesc     string
-	RollupCategoryCode   string
-	RollupCategoryDesc   string
-	GrantDate            string
-	ExpiredDate          string
-	CancellationDate     string
-	LastActionDate       string
-	LicStatusCode        string
-	LicStatusDesc        string
-	RollupStatusCode     string
-	RollupStatusDesc     string
-	EntityTypeCode       string
-	EntityTypeDesc       string
-	RollupEntityCode     string
-	RollupEntityDesc     string
-	LicAddress           string
-	LicCity              string
-	LicState             string
-	LicZipCode           string
-	LicAttentionLine     string
-	ContactCompany       string
-	ContactName          string
-	ContactTitle         string
-	ContactAddress1      string
-	ContactAddress2      string
-	ContactCity          string
-	ContactState         string
-	ContactZip           string
-	ContactCountry       string
-	ContactPhone         string
-	ContactFax           string
-	ContactEMail         string
-	MarketCode           string
-	MarketDesc           string
-	ChannelBlock         string
-	LocTypeCode          string
-	LocTypeDesc          string
-	LocCity              string
-	LocCountyCode        string
-	LocCountyName        string
-	LocState             string
-	LocRadiusOp          string
-	LocSeqID             string
-	LocLatDeg            string
-	LocLatMin            string
-	LocLatSec            string
-	LocLatDir            string
-	LocLongDeg           string
-	LocLongMin           string
-	LocLongSec           string
-	LocLongDir           string
-	HGTStructure         string
-	ASRNum               string
-	AntennaID            string
-	AntSeqID             string
-	AntMake              string
-	AntModel             string
-	AntTypeCode          string
-	AntTypeDesc          string
-	Azimuth              string
-	Beamwidth            string
-	PolarizationCode     string
-	FrequencyID          string
-	FreqSeqID            string
-	FreqClassStationCode string
-	FreqClassStationDesc string
-	PowerERP             string
-	PowerOutput          string
-	FrequencyAssigned    string
-	FrequencyUpperBand   string
-	UnitOfMeasure        string
-	Tolerance            string
-	EmissionID           string
-	EmissionSeqID        string
-	EmissionCode         string
-	GroundElevation      string
+	LicenseID          string
+	SourceSystem       string
+	Callsign           string
+	FacilityID         string
+	FRN                string
+	LicName            string
+	CommonName         string
+	RadioServiceCode   string
+	RadioServiceDesc   string
+	RollupCategoryCode string
+	RollupCategoryDesc string
+	GrantDate          string
+	ExpiredDate        string
+	CancellationDate   string
+	LastActionDate     string
+	LicStatusCode      string
+	LicStatusDesc      string
+	RollupStatusCode   string
+	RollupStatusDesc   string
+	EntityTypeCode     string
+	EntityTypeDesc     string
+	RollupEntityCode   string
+	RollupEntityDesc   string
+	LicAddress         string
+	LicCity            string
+	LicState           string
+	LicZipCode         string
+	LicAttentionLine   string
+	//	ContactCompany       string
+	//	ContactName          string
+	//	ContactTitle         string
+	//	ContactAddress1      string
+	//	ContactAddress2      string
+	//	ContactCity          string
+	//	ContactState         string
+	//	ContactZip           string
+	//	ContactCountry       string
+	//	ContactPhone         string
+	//	ContactFax           string
+	//	ContactEMail         string
+	//	MarketCode           string
+	//	MarketDesc           string
+	//	ChannelBlock         string
+	//	LocTypeCode          string
+	//	LocTypeDesc          string
+	//	LocCity              string
+	//	LocCountyCode        string
+	//	LocCountyName        string
+	//	LocState             string
+	//	LocRadiusOp          string
+	//	LocSeqID             string
+	//	LocLatDeg            string
+	//	LocLatMin            string
+	//	LocLatSec            string
+	//	LocLatDir            string
+	//	LocLongDeg           string
+	//	LocLongMin           string
+	//	LocLongSec           string
+	//	LocLongDir           string
+	//	HGTStructure         string
+	//	ASRNum               string
+	//	AntennaID            string
+	//	AntSeqID             string
+	//	AntMake              string
+	//	AntModel             string
+	//	AntTypeCode          string
+	//	AntTypeDesc          string
+	//	Azimuth              string
+	//	Beamwidth            string
+	//	PolarizationCode     string
+	//	FrequencyID          string
+	//	FreqSeqID            string
+	//	FreqClassStationCode string
+	//	FreqClassStationDesc string
+	//	PowerERP             string
+	//	PowerOutput          string
+	//	FrequencyAssigned    string
+	//	FrequencyUpperBand   string
+	//	UnitOfMeasure        string
+	//	Tolerance            string
+	//	EmissionID           string
+	//	EmissionSeqID        string
+	//	EmissionCode         string
+	//	GroundElevation      string
 }
